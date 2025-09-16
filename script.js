c1cb5818b2db2b666fabcc65673d06edc1be3b57
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-09-16 16:37:59 +0530

    click counter

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-var h1 = document.querySelector('h1')
+var btn1 = document.querySelector('#btn1')
+var btn2 = document.querySelector('#btn2')
+var sp = document.querySelector('h1 span')
 
 
-h1.addEventListener('click' , function() {
-    console.log('clicked')
-    h1.innerHTML = 'i am iron man';
-    h1.style.color = 'red';
-    h1.style.backgroundColor = 'black';
+var a = 0;
+btn1.addEventListener('click' , function() {
+    a++;
+    console.log(a)
+    sp.innerHTML = a;
+})
+
+btn2.addEventListener('click' , function() {
+    a--;
+    console.log(a)
+    sp.innerHTML = a;
 })
