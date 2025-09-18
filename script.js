a39aaf199e6caeed3c6abc89fdd2b256d3c725a4
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-09-18 20:45:24 +0530

    team guess

--- script.js
+++ script.js
@@ -1,24 +1,76 @@
-var h1 = document.querySelector('#task1 h1')
-var btn1 = document.querySelector('#btn1')
-var div = document.querySelector('#box')
-var btn2 = document.querySelector('#btn2')
-var rgb = document.querySelector('#box p')
+var h1 = document.querySelector("h1");
+var btn = document.querySelector("button");
+var h2 = document.querySelector("h2");
+var p = document.querySelector("p");
+
+var arr = [
+  {
+    teamName: "Manchester utd",
+    primary: "red",
+    secondary: "white",
+    trophies: "3",
+  },
+  {
+    teamName: "astion villa",
+    primary: "red",
+    secondary: "sky blue",
+    trophies: "0",
+  },
+  {
+    teamName: "tottenham hotspur",
+    primary: "white",
+    secondary: "blue",
+    trophies: "0",
+  },
+  {
+    teamName: "real Madrid",
+    primary: "black",
+    secondary: "white",
+    trophies: "15",
+  },
+  {
+    teamName: "Ac Milan",
+    primary: "red",
+    secondary: "black",
+    trophies: "7",
+  },
+  {
+    teamName: "Atletico de madrid",
+    primary: "red",
+    secondary: "white",
+    trophies: "3",
+  },
+  {
+    teamName: "arsenal",
+    primary: "red",
+    secondary: "violet",
+    trophies: "0",
+  },
+  {
+    teamName: "varca",
+    primary: "red",
+    secondary: "blue",
+    trophies: "5",
+  },
+  {
+    teamName: "Man city",
+    primary: "white",
+    secondary: "blue",
+    trophies: "x",
+  },
+  {
+    teamName: "juventus",
+    primary: "black",
+    secondary: "dark grey",
+    trophies: "2",
+  },
+];
 
 
-btn2.addEventListener('click', function() {
-
-    var t = Math.floor(Math.random()*90)
-    var l = Math.floor(Math.random()*90)
-    var d = Math.floor(Math.random()*360)
-
-    var r = Math.floor(Math.random()*225)
-    var g = Math.floor(Math.random()*225)
-    var b = Math.floor(Math.random()*225)
-
-    div.style.top = t + '%'
-    div.style.left = l + '%'
-    div.style.rotate = d +'deg'
-
-    div.style.backgroundColor = `rgb(${r} ,${g} ,${b})`
-    rgb.textContent = `R: ${r}, G: ${g}, B: ${b}`
-})
+btn.addEventListener("click", function () {
+  var idx = Math.floor(Math.random() * arr.length);
+  h2.innerText = arr[idx].teamName;
+  h2.style.backgroundColor = arr[idx].secondary;
+  h2.style.color = arr[idx].primary;
+  p.innerHTML = arr[idx].trophies + ' :titles';
+});
