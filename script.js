00a0d5682f30eeaecd370993aa6733a6b124fdd6
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-09-18 16:35:10 +0530

    floating div

--- script.js
+++ script.js
@@ -5,16 +5,19 @@ var btn2 = document.querySelector('#btn2')
 var rgb = document.querySelector('#box p')
 
 
-btn1.addEventListener('click', function() {
-    var a = Math.floor(Math.random()*10)
-    h1.innerHTML = a;
-})
-
 btn2.addEventListener('click', function() {
+
+    var t = Math.floor(Math.random()*90)
+    var l = Math.floor(Math.random()*90)
+    var d = Math.floor(Math.random()*360)
 
     var r = Math.floor(Math.random()*225)
     var g = Math.floor(Math.random()*225)
     var b = Math.floor(Math.random()*225)
+
+    div.style.top = t + '%'
+    div.style.left = l + '%'
+    div.style.rotate = d +'deg'
 
     div.style.backgroundColor = `rgb(${r} ,${g} ,${b})`
     rgb.textContent = `R: ${r}, G: ${g}, B: ${b}`
