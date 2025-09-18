90555e0d83971782649a50fe43749c5684c12d6f
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-09-18 16:10:01 +0530

    17.9

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-var h1 = document.querySelector('h1')
-var h2 = document.querySelector('h2')
-var h3 = document.querySelector('h3')
+
+var div = document.querySelector('#box')
 var btn1 = document.querySelector('#btn1')
+var btn2 = document.querySelector('#btn2')
 
 
 
 btn1.addEventListener('click' , function() {
-    h1.textContent = 'alex'
-    h2.textContent = 18
-    h3.style.color = 'red'
-    h3.textContent = 'red'
+    div.style.backgroundColor = 'yellow'
 })
+
+btn2.addEventListener('click' , function() {
+    div.style.backgroundColor = 'transparent'
+})
+
 
