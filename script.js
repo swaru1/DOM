f6a5068fae6d1bfaa7b2bddaa7e9f4df44c9394c
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-09-19 15:29:39 +0530

    updated array of objects

--- script.js
+++ script.js
@@ -3,74 +3,99 @@ var btn = document.querySelector("button");
 var h2 = document.querySelector("h2");
 var p = document.querySelector("p");
 
-var arr = [
+const arr = [
   {
-    teamName: "Manchester utd",
-    primary: "red",
-    secondary: "white",
-    trophies: "3",
+    teamName: "Real Madrid",
+    color1: "#ffffff", // White
+    color2: "#004080", // Dark Blue
+    color3: "black", // Gold (crest)
+    trophies: 15
   },
   {
-    teamName: "astion villa",
-    primary: "red",
-    secondary: "sky blue",
-    trophies: "0",
+    teamName: "AC Milan",
+    color1: "#ff0000", // Red
+    color2: "#000000", // Black
+    color3: "#ffffff", // White
+    trophies: 7
   },
   {
-    teamName: "tottenham hotspur",
-    primary: "white",
-    secondary: "blue",
-    trophies: "0",
+    teamName: "Bayern Munich",
+    color1: "#dc052d", // Red
+    color2: "#4e1409ff", // Blue
+    color3: "#ffffff", // White
+    trophies: 6
   },
   {
-    teamName: "real Madrid",
-    primary: "black",
-    secondary: "white",
-    trophies: "15",
+    teamName: "Liverpool",
+    color1: "#c8102e", // Red
+    color2: "#00b2a9", // Teal (crest detail)
+    color3: "#ffffff", // White
+    trophies: 6
   },
   {
-    teamName: "Ac Milan",
-    primary: "red",
-    secondary: "black",
-    trophies: "7",
+    teamName: "Barcelona",
+    color1: "#004d98", // Blaugrana Blue
+    color2: "#a50044", // Blaugrana Red
+    color3: "#ffed02", // Yellow
+    trophies: 5
   },
   {
-    teamName: "Atletico de madrid",
-    primary: "red",
-    secondary: "white",
-    trophies: "3",
+    teamName: "Ajax",
+    color1: "#ffffff", // White
+    color2: "#642626ff",
+    color3: "#e30613", // Red
+    trophies: 4
   },
   {
-    teamName: "arsenal",
-    primary: "red",
-    secondary: "violet",
-    trophies: "0",
+    teamName: "Inter Milan",
+    color1: "#000000", // Black
+    color2: "#0033a0", // Blue  
+    trophies: 3
   },
   {
-    teamName: "varca",
-    primary: "red",
-    secondary: "blue",
-    trophies: "5",
+    teamName: "Manchester United",
+    color1: "#e2ceccff", // Red
+    color2: "#c31a15ff", // Yellow
+    color3: "#000000", // Black
+    trophies: 3
   },
   {
-    teamName: "Man city",
-    primary: "white",
-    secondary: "blue",
-    trophies: "x",
+    teamName: "Chelsea",
+    color1: "#ffffff", // White
+    color2: "#034694", // Blue
+    color3: "#e6c300", // Gold
+    trophies: 2
   },
   {
-    teamName: "juventus",
-    primary: "black",
-    secondary: "dark grey",
-    trophies: "2",
+    teamName: "Benfica",
+    color1: "#ffffff", // White
+    color2: "#e30613", // Red
+    color3: "#f6d500", // Yellow
+    trophies: 2
   },
+  {
+    teamName: "Juventus",
+    color1: "#000000", // Black
+    color2: "#4e4d4dff", // White
+    color3: "#d4af37", // Gold (crest)
+    trophies: 2
+  },
+  {
+    teamName: "Porto",
+    color1: "#ffffff", // White
+    color2: "#002f6c", // Blue
+    color3: "#cf142b", // Red (crest)
+    trophies: 2
+  }
 ];
 
 
 btn.addEventListener("click", function () {
   var idx = Math.floor(Math.random() * arr.length);
   h2.innerText = arr[idx].teamName;
-  h2.style.backgroundColor = arr[idx].secondary;
+  h2.style.background = `linear-gradient(90deg, ${arr[idx].color1}, ${arr[idx].color2}, ${arr[idx].color3})`;
   h2.style.color = arr[idx].primary;
   p.innerHTML = arr[idx].trophies + ' :titles';
 });
+
+
