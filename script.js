d6bfd08e49551a956d783d95eeb81b3959100dde
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-09-22 17:07:17 +0530

    swap two images

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
+var img1 = document.querySelector('#img1')
+var img2 = document.querySelector('#img2')
 var btn = document.querySelector('.btn')
-var bulb = document.querySelector('#bulb')
+
+var src1 = img1.getAttribute('src')
+var src2 = img2.getAttribute('src')
 
 var flag = 0
 
 btn.addEventListener('click', function(){
+  console.log('button click')
   if(flag == 0) {
-    bulb.style.backgroundColor = 'yellow'
-    btn.innerHTML = 'OFF'
+    img1.setAttribute('src', src2)
+    img2.setAttribute('src', src1)
     flag = 1;
   } else {
-    bulb.style.backgroundColor = 'white'
-    btn.innerHTML = 'ON'
+    img1.setAttribute('src', src1)
+    img2.setAttribute( 'src', src2)
     flag = 0;
   }
 })
