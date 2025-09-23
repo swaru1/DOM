74f4d4d52fc8cd6ebe78f8b63c4628640a67cfc8
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-09-23 17:01:49 +0530

    custom cursor

--- script.js
+++ script.js
@@ -1,21 +1,12 @@
-var img = document.querySelector('img')
-var btn = document.querySelector('.btn')
-var cont = document.querySelector('.container')
+var h1 = document.querySelector('h1 span')
+var h2 = document.querySelector('h2 span')
+var body = document.querySelector('body')
+var cur = document.querySelector('#cursor')
 
-
-
-btn.addEventListener('click', function(){
-
-  var t = Math.random() * 100;
-  var l = Math.random() * 100;
-  var rot = Math.random() * 360;
-  var h = Math.random() * 300;
-
-  var img = document.createElement('img')
-  img.setAttribute('src', 'https://www.pinclipart.com/picdir/big/30-304178_mouse-free-png-transparent-image-cartoons-pinterest-clipart.png')
-  img.style.top = t+'%';
-  img.style.left = l+'%';
-  img.style.rotate = rot+'deg';
-  img.style.height = h+'px';
-  cont.appendChild(img)  
+body.addEventListener('mousemove', function(dets){
+  
+  cur.style.top = dets.y + 'px'
+  cur.style.left = dets.x + 'px'
 })
+
+
