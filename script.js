732b938964f9ec77ccfbba702a976ed92ee275c8
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-09-23 21:40:47 +0530

    cursor hover heading effect

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-var h1 = document.querySelector('h1 span')
-var h2 = document.querySelector('h2 span')
+var h1 = document.querySelector('h1')
 var body = document.querySelector('body')
 var cur = document.querySelector('#cursor')
 
 body.addEventListener('mousemove', function(dets){
-  
   cur.style.top = dets.y + 'px'
   cur.style.left = dets.x + 'px'
+})
+
+h1.addEventListener( 'mouseenter' , function() {
+  cur.style.scale = 3
+})
+
+h1.addEventListener( 'mouseleave' , function() {
+  cur.style.scale = 1
 })
 
 
