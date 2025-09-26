3367dcfef2c61428b94f1a1d224bef84f7b13a15
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-09-26 20:06:35 +0530

    move img with arrow keys

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-var h1 = document.querySelector('h1')
-var body = document.querySelector('body')
-var cur = document.querySelector('#cursor')
+var h1 = document.querySelector("h1");
+var body = document.querySelector("body");
+var img = document.querySelector("img");
 
-body.addEventListener('mousemove', function(dets){
-  cur.style.top = dets.y + 'px'
-  cur.style.left = dets.x + 'px'
-})
+var numX = 0;
+var numY = 0;
 
-h1.addEventListener( 'mouseenter' , function() {
-  cur.style.scale = 3
-})
+document.addEventListener("keydown", function (dets) {
+  if (dets.code == "ArrowRight") {
+    numX += 10
+    img.style.left = numX + "px";
+  }
 
-h1.addEventListener( 'mouseleave' , function() {
-  cur.style.scale = 1
-})
+  if (dets.code == "ArrowLeft") {
+    numX -= 10
+    img.style.left = numX + "px";
+  }
 
+  if (dets.code == "ArrowUp") {
+    numY -= 10
+    img.style.top = numY + "px";
+  }
 
+  if (dets.code == "ArrowDown") {
+    numY += 10
+    img.style.top = numY + "px";
+  }
+
+  h1.innerHTML = dets.code;
+});
