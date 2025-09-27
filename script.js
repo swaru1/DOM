998b92277de41c24fd2ba2ca9514d2e4b1043f9d
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-09-27 16:57:21 +0530

    change text on click

--- script.js
+++ script.js
@@ -1,30 +1,9 @@
-var h1 = document.querySelector("h1");
-var body = document.querySelector("body");
-var img = document.querySelector("img");
+var allDiv = document.querySelectorAll('.text')
+var output = document.querySelector('.output')
 
-var numX = 0;
-var numY = 0;
-
-document.addEventListener("keydown", function (dets) {
-  if (dets.code == "ArrowRight") {
-    numX += 10
-    img.style.left = numX + "px";
-  }
-
-  if (dets.code == "ArrowLeft") {
-    numX -= 10
-    img.style.left = numX + "px";
-  }
-
-  if (dets.code == "ArrowUp") {
-    numY -= 10
-    img.style.top = numY + "px";
-  }
-
-  if (dets.code == "ArrowDown") {
-    numY += 10
-    img.style.top = numY + "px";
-  }
-
-  h1.innerHTML = dets.code;
-});
+allDiv.forEach(function(elem){
+  elem.addEventListener('click', function(){
+    console.log('clicked')
+    output.innerHTML = elem.textContent
+  })
+})
