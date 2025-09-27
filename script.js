3b3a87a1b4eb2f8891da476a08714f6bff9d711f
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-09-27 23:08:49 +0530

    playground random image positions

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-var allDiv = document.querySelectorAll('.text')
-var output = document.querySelector('.output')
+var images = document.querySelectorAll('.images img')
+var playground = document.querySelector('.playground')
 
-allDiv.forEach(function(elem){
-  elem.addEventListener('click', function(){
-    console.log('clicked')
-    output.innerHTML = elem.textContent
+images.forEach(function(image){
+  image.addEventListener('click', function(){
+
+    var t = Math.random() * 90;
+    var l = Math.random() * 90;
+    var rot = Math.random() * 360;
+    var s  = Math.random() * 5;
+
+    var link = image.getAttribute('src')
+    console.log(link)
+
+    var newImg = document.createElement('img')
+    newImg.setAttribute('src', link)
+
+    newImg.style.position = 'absolute'
+    newImg.style.top = t + '%';
+    newImg.style.left = l + '%';
+    newImg.style.rotate = rot + 'deg';
+    newImg.style.scale = s;
+    playground.appendChild(newImg)  
   })
 })
