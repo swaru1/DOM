c5e4c1abd1d9e448189155224109c321c478478b
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-09-29 16:40:49 +0530

    average marks

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-var arr = [10, 20, 30, 40];
-var sum = 0;
-arr.forEach(function (elem) {
-  sum += elem;
-});
-console.log(sum);
+var obj = [
+  {
+    student: "sarthak",
+    marks: 90,
+  },
+  {
+    student: "Akash",
+    marks: 50,
+  },
+  {
+    student: "sumit",
+    marks: 80,
+  },
+];
 
+var sum = 0;
+var avg = 0;
+
+obj.forEach(function (elem) {
+  sum += elem.marks;
+  avg = sum / obj.length;
+});
+
+console.log(avg);
