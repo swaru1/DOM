2fda040f71a663e145cf344e0c16f4609372accc
Somwanshi Swaraj <somwanshiswaraj4@gmail.com>
2025-09-29 16:32:01 +0530

    task : sum

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
-var images = document.querySelectorAll('.images img')
-var playground = document.querySelector('.playground')
+var arr = [10, 20, 30, 40];
+var sum = 0;
+arr.forEach(function (elem) {
+  sum += elem;
+});
+console.log(sum);
 
-images.forEach(function(image){
-  image.addEventListener('click', function(){
-
-    var t = Math.random() * 90;
-    var l = Math.random() * 90;
-    var rot = Math.random() * 360;
-    var s  = Math.random() * 5;
-
-    var link = image.getAttribute('src')
-    console.log(link)
-
-    var newImg = document.createElement('img')
-    newImg.setAttribute('src', link)
-
-    newImg.style.position = 'absolute'
-    newImg.style.top = t + '%';
-    newImg.style.left = l + '%';
-    newImg.style.rotate = rot + 'deg';
-    newImg.style.scale = s;
-    playground.appendChild(newImg)  
-  })
-})
